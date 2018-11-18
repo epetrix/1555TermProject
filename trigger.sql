94365cb06c7efefaa92e945fd8a75f31ac08c0f9
Kyle Amoroso <kamoroso1994@gmail.com>
2018-11-17 19:44:59 -0500

    Finish first function

--- trigger.sql
+++ trigger.sql
@@ -1,17 +1,19 @@
 -- functions
-create or replace FUNCTION func_productCount(c IN varchar2, x IN number)
-  RETURN number
-  IS total number;
-
+CREATE OR REPLACE FUNCTION func_productCount(c IN varchar2, x IN number)
+  RETURN number IS
+  total number;
   BEGIN
-    SELECT c_date - sell_date AS months, category, count(*) INTO total
-      FROM Product NATURAL JOIN BelongsTo JOIN ourSysDATE
-      WHERE sell_date IS NOT NULL AND c = category AND months <= x
-      GROUP BY months, category; -- auction_id?
+    SELECT count(*) INTO total
+      FROM Product P NATURAL JOIN BelongsTo B CROSS JOIN ourSysDATE O
+      WHERE P.sell_date IS NOT NULL
+        AND B.category = c
+        AND MONTHS_BETWEEN(O.c_date, P.sell_date) <= x;
 
-    RETURN(total);
+    RETURN total;
   END;
-
-CREATE OR REPLACE FUNCTION func_bidCount(u IN varchar2, x IN number)
-
-CREATE OR REPLACE FUNCTION func_buyingAmount(u IN varchar2, x IN number)
+/
+-- CREATE OR REPLACE FUNCTION func_bidCount(u IN varchar2, x IN number)
+-- /
+-- CREATE OR REPLACE FUNCTION func_buyingAmount(u IN varchar2, x IN number)
+-- /
+commit;
