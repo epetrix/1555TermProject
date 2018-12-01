846f04b889e1ba6cc1fbacea1093eeb873eb9ecd
Kyle Amoroso <kamoroso1994@gmail.com>
2018-12-01 18:59:30 -0500

    Begin admin interface

---
+++ Administrator.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Administrator {
+  private static final Scanner input = new Scanner(System.in);
+
+  private static displayMenu() {
+    System.out.println("***************************");
+    System.out.println("* Administrator Interface *");
+    System.out.println("***************************");
+    System.out.println("a) New customer registration");
+    System.out.println("b) Update system date");
+    System.out.println("c) Product statistics");
+    System.out.println("d) Exit menu");
+    System.out.println();
+  }
+
+  private static char getChoice(char start, char end) {
+    char answer;
+
+    System.out.print("Choice: ");
+    while(true) {
+      String line = input.nextLine();
+      if(line.length == 1) {
+        answer = line.charAt(0);
+        if(answer >= start && answer <= end) break;
+      }
+
+      System.out.println("Answer must fall between '" + start + "' and '" + end + "'");
+      System.out.print("Choice: ");
+    }
+    return answer;
+  }
+
+  public static void menu() {
+    char answer;
+    do {
+      displayMenu();
+      answer = getChoice('a', 'd');
+      switch(answer) {
+        case 'a':
+        registerCustomer();
+        break;
+
+        case 'b':
+        updateSysDate();
+        break;
+
+        case 'c':
+        getProductStats();
+        break;
+
+        case 'd':
+        System.out.println("Exiting admin interface");
+        break;
+      }
+      Sytem.out.println();
+    } while(answer != 'd');
+  }
+
+  private static void registerCustomer() {
+    System.out.println("Registering customer...");
+  }
+
+  private static void updateSysDate() {
+    System.out.println("Updating system date...");
+  }
+
+  private static void getProductStats() {
+    System.out.println("Getting product statistics...");
+  }
+}
